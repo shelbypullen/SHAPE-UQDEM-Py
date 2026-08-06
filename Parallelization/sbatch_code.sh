aba1bd0ad20f5b942c28bb634c62593ff0a4a79b
Shelby Katherine Pullen <pullen3@dane5.llnl.gov>
2026-08-06 09:40:27 -0700

    500x500 grid results

--- Parallelization/sbatch_code.sh
+++ Parallelization/sbatch_code.sh
@@ -3,11 +3,11 @@
 #SBATCH --output=results_%j.out
 #SBATCH --error=results_%j.err
 #SBATCH --nodes=1
-#SBATCH --ntasks=10
+#SBATCH --ntasks=90
 #SBATCH --cpus-per-task=1
-#SBATCH --time=00:10:00
-#SBATCH --partition=pdebug
-#SBATCH --mail-type=END
+#SBATCH --time=00:40:00
+#SBATCH --partition=pbatch
+#SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=spullen@ucsd.edu
 
 cd $SLURM_SUBMIT_DIR
