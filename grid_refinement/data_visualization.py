b58211c663f0d89762ceffa4d6d25aa1799a4e9f
Shelby Pullen <spullen@ucsd.edu>
2026-08-06 18:24:40 -0700

    change to oval to avoid unnecessary computation

--- grid_refinement/data_visualization.py
+++ grid_refinement/data_visualization.py
@@ -5,35 +5,12 @@ from matplotlib import colors
 
 script_dir = os.path.dirname(__file__)                  # get the current file path to grab all data here
 
-# load most of the data
+#load data
 c_nums = np.load(os.path.join(script_dir, "c_nums.npy"))
 total_KE_avgs = np.load(os.path.join(script_dir, "total_KE_avgs.npy"),allow_pickle=True)
 total_KE_stds = np.load(os.path.join(script_dir, "total_KE_stds.npy"),allow_pickle=True)
 total_KE_cost = np.load(os.path.join(script_dir, "total_KE_cost.npy"),allow_pickle=True)
 total_KE_ratios = np.load(os.path.join(script_dir, "total_KE_ratios.npy"),allow_pickle=True)
-
-# load the 500x500 data
-script_dir500 = os.path.join(script_dir, "results_2026-08-06_14:45:12")
-KE_avgs = np.load(os.path.join(script_dir500, "KE_avgs.npy"),allow_pickle=True)
-KE_avgs500 = np.empty(1, dtype=object)
-KE_avgs500[0] = KE_avgs
-KE_stds = np.load(os.path.join(script_dir500, "KE_stds.npy"),allow_pickle=True)
-KE_stds500 = np.empty(1, dtype=object)
-KE_stds500[0] = KE_stds
-KE_cost = np.load(os.path.join(script_dir500, "KE_cost.npy"),allow_pickle=True)
-KE_cost500 = np.empty(1, dtype=object)
-KE_cost500[0] = KE_cost
-KE_ratios = np.load(os.path.join(script_dir500, "KE_ratios.npy"),allow_pickle=True)
-KE_ratios500 = np.empty(1, dtype=object)
-KE_ratios500[0] = KE_ratios
-
-# merge data
-c_nums = np.append(c_nums, int(500))
-total_KE_avgs = np.append(total_KE_avgs, KE_avgs500)
-total_KE_stds = np.append(total_KE_stds, KE_stds500)
-total_KE_cost = np.append(total_KE_cost, KE_cost500)
-total_KE_ratios = np.append(total_KE_ratios, KE_ratios500)
-
 
 # confirm that all arrays are the same size
 print(c_nums)
@@ -44,9 +21,6 @@ print(np.size(total_KE_ratios))
 
 print(np.size(total_KE_ratios[0]))
 
-######################################################
-# pcolor mesh of c2 c3 domain
-######################################################
 # the grid size should be as close to square as possible so round up the sqrt(length(c_nums))
 grid_size = int(np.ceil(np.sqrt(int(np.size(c_nums)))))
 print('plot grid size = ', grid_size)
@@ -76,7 +50,7 @@ for i in range(len(axs1)):
     norm = colors.TwoSlopeNorm(vmin=np.nanmin(total_KE_avgs[i]), vcenter=1, vmax = np.nanmax(total_KE_avgs[i]))
 
     # plot the graph
-    pcm1 = axs1[i].pcolormesh(c3,c2, total_KE_avgs[i],cmap='RdBu_r',norm=norm)
+    pcm1 = axs1[i].pcolormesh(c2,c3, total_KE_avgs[i],cmap='RdBu_r',norm=norm)
     axs1[i].set_title(f"{int(c_nums[i])}x{int(c_nums[i])}")
     axs1[i].set_xlabel("C3", fontsize=8)
     axs1[i].set_ylabel("c2", fontsize=8)
@@ -90,20 +64,16 @@ fig1.savefig(file_path_fig1, dpi=300, bbox_inches='tight')
 # plot the highest resolution 
 fig2, axs2 = plt.subplots(1,1,figsize=(2*(grid_size), 2*(grid_size)))
 norm2 = colors.TwoSlopeNorm(vmin=np.nanmin(total_KE_avgs[-1]), vcenter=1, vmax = np.nanmax(total_KE_avgs[-1]))
-pcm2 = axs2.pcolormesh(c3,c2, total_KE_avgs[-1].T,cmap='RdBu_r',norm=norm2)
+pcm2 = axs2.pcolormesh(c2,c3, total_KE_avgs[-1],cmap='RdBu_r',norm=norm2)
 axs2.set_title(f"{int(c_nums[-1])}x{int(c_nums[-1])}")
 axs2.set_xlabel("C3", fontsize=8)
-axs2.set_ylabel("C2", fontsize=8)
+axs2.set_ylabel("c2", fontsize=8)
 fig2.colorbar(pcm2, ax=axs2).set_label("KE Ratio", fontsize=6)
 plt.tight_layout()
 
 # save the plot
 file_path_fig2 = os.path.join(script_dir, f"most_refined_{c_nums[-1]}x{c_nums[-1]}_KE_avgs.png")
 fig2.savefig(file_path_fig2, dpi=300, bbox_inches='tight')
-
-######################################################
-# opt C2 and C3
-######################################################
 
 # to see the plot
 plt.show()
