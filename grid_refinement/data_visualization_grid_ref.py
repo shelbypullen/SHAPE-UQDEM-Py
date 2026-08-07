67177543dc8e9e6c94bfc5196b59710172faab9e
Shelby Pullen <spullen@ucsd.edu>
2026-08-07 12:04:47 -0700

    improve grid visualization scripts

--- grid_refinement/data_visualization_grid_ref.py
+++ grid_refinement/data_visualization_grid_ref.py
@@ -3,7 +3,13 @@ import os
 from matplotlib import pyplot as plt
 from matplotlib import colors
 
+"""
+COPY THIS SCRIPT INTO THE RESULTS FOLDER OR ADD ITS FILE PATH TO THE script_dir
+"""
+
 script_dir = os.path.dirname(__file__)                  # get the current file path to grab all data here
+figure_dir = os.path.join(script_dir, "Figures")
+os.makedirs(figure_dir, exist_ok=True)
 
 #load data
 c_nums = np.load(os.path.join(script_dir, "c_nums.npy"))
@@ -58,7 +64,7 @@ for i in range(len(axs1)):
 
 # save the figure
 plt.tight_layout()
-file_path_fig1 = os.path.join(script_dir, f"KE_avgs_grid_ref_{len(c_nums)}_steps.png")
+file_path_fig1 = os.path.join(figure_dir, f"KE_avgs_grid_ref_{len(c_nums)}_steps.png")
 fig1.savefig(file_path_fig1, dpi=300, bbox_inches='tight')
 
 # plot the highest resolution 
@@ -72,8 +78,61 @@ fig2.colorbar(pcm2, ax=axs2).set_label("KE Ratio", fontsize=6)
 plt.tight_layout()
 
 # save the plot
-file_path_fig2 = os.path.join(script_dir, f"most_refined_{c_nums[-1]}x{c_nums[-1]}_KE_avgs.png")
+file_path_fig2 = os.path.join(figure_dir, f"most_refined_{c_nums[-1]}x{c_nums[-1]}_KE_avgs.png")
 fig2.savefig(file_path_fig2, dpi=300, bbox_inches='tight')
+
+# to see the plot
+plt.show()
+
+######################################################
+# opt value graphs
+######################################################
+opt_c2s = np.zeros(len(c_nums))
+opt_c3s = np.zeros(len(c_nums))
+
+opt_KEs = np.zeros((len(c_nums),3))
+opt_KE_avgs = np.zeros((len(c_nums),3))
+opt_KE_std = np.zeros((len(c_nums),3))
+
+# grab all optimal values for plotting
+for i in range(len(c_nums)):
+
+    c2 = np.linspace(-10,-1,c_nums[i])
+    c3 = np.linspace(1,15,c_nums[i])
+
+    # get optimal values
+    c2_idx,c3_idx = np.unravel_index(np.nanargmin(total_KE_cost[i]), total_KE_cost[i].shape)      # where the optimal spring coefficients are
+    opt_c2s[i] = c2[c2_idx]
+    opt_c3s[i] = c3[c3_idx]
+    opt_KEs[i] = [np.nanmin(total_KE_cost[i]), total_KE_avgs[i][c2_idx,c3_idx], total_KE_stds[i][c2_idx,c3_idx]]
+    
+    c2_idx,c3_idx = np.unravel_index(np.nanargmin(total_KE_avgs[i]), total_KE_avgs[i].shape)      # where best avg KE ratio is
+    opt_KE_avgs[i] = [np.nanmin(total_KE_avgs[i]), c2[c2_idx], c3[c3_idx]]
+    
+    c2_idx,c3_idx = np.unravel_index(np.nanargmin(total_KE_stds[i]), total_KE_stds[i].shape)      # where best std of KE ratio is
+    opt_KE_std[i] = [np.nanmin(total_KE_stds[i]), c2[c2_idx], c3[c3_idx]]
+
+# plotting optimal spring coefficients for cost function
+fig3, ax3 = plt.subplots(1,1,figsize=(2*(grid_size), (grid_size)))
+ax3.plot(c_nums, opt_c2s)
+ax3.plot(c_nums, opt_c3s)
+ax3.legend(labels=["Optimal C2 Values","Optimal C3 Values"])
+ax3.set_title("Change in Optimal Coefficients as Resolution Improves")
+
+file_path_fig3 = os.path.join(figure_dir, "optimal_coeff_values_grid_ref.png")
+fig3.savefig(file_path_fig3, dpi=300, bbox_inches='tight')
+
+# plotting optimal KE values
+fig4, ax4 = plt.subplots(1,1, figsize=(2*(grid_size), grid_size))
+ax4.plot(c_nums, opt_KEs[:,0])
+ax4.plot(c_nums, opt_KE_avgs[:,0])
+ax4.plot(c_nums,opt_KE_std[:,0])
+ax4.legend(labels=["Min Cost Value","Min Average KE ratio","Min std of KE ratios"])
+ax4.set_title("Optimal Values as Grid resultion Improves")
+
+# saving the plot
+file_path_fig4 = os.path.join(figure_dir, "optimal_KE_values_grid_ref.png")
+fig4.savefig(file_path_fig4, dpi=300, bbox_inches='tight')
 
 # to see the plot
 plt.show()
--- grid_refinement/results_2026-08-03_23:05:47/data_visualization_grid_ref.py
+++ grid_refinement/results_2026-08-03_23:05:47/data_visualization_grid_ref.py
@@ -2,43 +2,21 @@ import numpy as np
 import os
 from matplotlib import pyplot as plt
 from matplotlib import colors
-from matplotlib.patches import Circle
+
 """
 COPY THIS SCRIPT INTO THE RESULTS FOLDER OR ADD ITS FILE PATH TO THE script_dir
 """
-script_dir = os.path.dirname(__file__)                  # pwd to grab all data in the same folder
+
+script_dir = os.path.dirname(__file__)                  # get the current file path to grab all data here
 figure_dir = os.path.join(script_dir, "Figures")
 os.makedirs(figure_dir, exist_ok=True)
 
-# load most of the data
+#load data
 c_nums = np.load(os.path.join(script_dir, "c_nums.npy"))
 total_KE_avgs = np.load(os.path.join(script_dir, "total_KE_avgs.npy"),allow_pickle=True)
 total_KE_stds = np.load(os.path.join(script_dir, "total_KE_stds.npy"),allow_pickle=True)
 total_KE_cost = np.load(os.path.join(script_dir, "total_KE_cost.npy"),allow_pickle=True)
 total_KE_ratios = np.load(os.path.join(script_dir, "total_KE_ratios.npy"),allow_pickle=True)
-
-# load the 500x500 data
-script_dir500 = os.path.join(script_dir, "results_2026-08-06_14:45:12")
-KE_avgs = np.load(os.path.join(script_dir500, "KE_avgs.npy"),allow_pickle=True)
-KE_avgs500 = np.empty(1, dtype=object)
-KE_avgs500[0] = KE_avgs
-KE_stds = np.load(os.path.join(script_dir500, "KE_stds.npy"),allow_pickle=True)
-KE_stds500 = np.empty(1, dtype=object)
-KE_stds500[0] = KE_stds
-KE_cost = np.load(os.path.join(script_dir500, "KE_cost.npy"),allow_pickle=True)
-KE_cost500 = np.empty(1, dtype=object)
-KE_cost500[0] = KE_cost
-KE_ratios = np.load(os.path.join(script_dir500, "KE_ratios.npy"),allow_pickle=True)
-KE_ratios500 = np.empty(1, dtype=object)
-KE_ratios500[0] = KE_ratios
-
-# merge data
-c_nums = np.append(c_nums, int(500))
-total_KE_avgs = np.append(total_KE_avgs, KE_avgs500)
-total_KE_stds = np.append(total_KE_stds, KE_stds500)
-total_KE_cost = np.append(total_KE_cost, KE_cost500)
-total_KE_ratios = np.append(total_KE_ratios, KE_ratios500)
-
 
 # confirm that all arrays are the same size
 print(c_nums)
@@ -49,15 +27,12 @@ print(np.size(total_KE_ratios))
 
 print(np.size(total_KE_ratios[0]))
 
-######################################################
-# pcolor mesh of c2 c3 domain
-######################################################
 # the grid size should be as close to square as possible so round up the sqrt(length(c_nums))
 grid_size = int(np.ceil(np.sqrt(int(np.size(c_nums)))))
 print('plot grid size = ', grid_size)
 
 # if statement to get rid of extra rows in subplot grid if necessary
-if grid_size**2 - grid_size >= len(c_nums):
+if grid_size**2 - grid_size > len(c_nums):
     fig1, axs1 = plt.subplots(grid_size-1,grid_size, figsize=(4*(grid_size), 3*(grid_size-1)))
 else:
     fig1, axs1 = plt.subplots(grid_size,grid_size, figsize=(3*grid_size, 3*grid_size))
@@ -81,18 +56,11 @@ for i in range(len(axs1)):
     norm = colors.TwoSlopeNorm(vmin=np.nanmin(total_KE_avgs[i]), vcenter=1, vmax = np.nanmax(total_KE_avgs[i]))
 
     # plot the graph
-    pcm1 = axs1[i].pcolormesh(c3,c2, total_KE_avgs[i],cmap='RdBu_r',norm=norm)
+    pcm1 = axs1[i].pcolormesh(c2,c3, total_KE_avgs[i],cmap='RdBu_r',norm=norm)
     axs1[i].set_title(f"{int(c_nums[i])}x{int(c_nums[i])}")
     axs1[i].set_xlabel("C3", fontsize=8)
     axs1[i].set_ylabel("c2", fontsize=8)
     fig1.colorbar(pcm1, ax=axs1[i]).set_label("KE Ratio", fontsize=6)
-
-    # red circle around optimal solution
-    c2_idx,c3_idx = np.unravel_index(np.nanargmin(total_KE_cost[i]), total_KE_cost[i].shape)
-    c2_opt = c2[c2_idx]
-    c3_opt = c3[c3_idx]
-    circle = plt.Circle((c3_opt,c2_opt), radius=0.5, color='red', fill=False, linewidth=2)
-    axs1[i].add_patch(circle)
 
 # save the figure
 plt.tight_layout()
@@ -102,16 +70,19 @@ fig1.savefig(file_path_fig1, dpi=300, bbox_inches='tight')
 # plot the highest resolution 
 fig2, axs2 = plt.subplots(1,1,figsize=(2*(grid_size), 2*(grid_size)))
 norm2 = colors.TwoSlopeNorm(vmin=np.nanmin(total_KE_avgs[-1]), vcenter=1, vmax = np.nanmax(total_KE_avgs[-1]))
-pcm2 = axs2.pcolormesh(c3,c2, total_KE_avgs[-1],cmap='RdBu_r',norm=norm2)
+pcm2 = axs2.pcolormesh(c2,c3, total_KE_avgs[-1],cmap='RdBu_r',norm=norm2)
 axs2.set_title(f"{int(c_nums[-1])}x{int(c_nums[-1])}")
 axs2.set_xlabel("C3", fontsize=8)
-axs2.set_ylabel("C2", fontsize=8)
+axs2.set_ylabel("c2", fontsize=8)
 fig2.colorbar(pcm2, ax=axs2).set_label("KE Ratio", fontsize=6)
 plt.tight_layout()
 
 # save the plot
 file_path_fig2 = os.path.join(figure_dir, f"most_refined_{c_nums[-1]}x{c_nums[-1]}_KE_avgs.png")
 fig2.savefig(file_path_fig2, dpi=300, bbox_inches='tight')
+
+# to see the plot
+plt.show()
 
 ######################################################
 # opt value graphs
