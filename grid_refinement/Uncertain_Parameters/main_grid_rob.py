f5a693470ba81812b0f1afb0c8d3b77cd207437e
Shelby Pullen <spullen@ucsd.edu>
2026-08-11 14:51:52 -0700

    rearranged some things and started on multinode script

--- grid_refinement/Uncertain_Parameters/main_grid_rob.py
+++ grid_refinement/Uncertain_Parameters/main_grid_rob.py
@@ -7,12 +7,8 @@ import os                                   # to get number of cpus
 import time                                 # to track computational time
 from datetime import datetime
 
-from matplotlib import pyplot as plt
-from matplotlib import colors
-from matplotlib.patches import Ellipse
-
 """
-!!!!!!MAKE SURE TO CHANGE COARSENESS FOR LAPTOP RUNS!!!!!!
+!!!!ONLY USE ON HPC BECAUSE OF MULTINODAL STRUCTURE!!!!
 """
 
 ############################################################
@@ -44,18 +40,6 @@ def run_obj(i, j, c2, c3, n_samples):
         KE_cost = np.nan
         return i, j, KE_cost, KE_avgs, KE_stds, np.full(n_samples, np.nan)
     
-    theta = 295*np.pi/180
-    a = 3
-    b = 7
-    c = 6
-    d = -4
-
-    if ((c3-c)*np.cos(theta) - (c2-d)*np.sin(theta))**2/(a**2) + ((c3-c)*np.sin(theta) + (c2-d)*np.cos(theta))**2/(b**2) > 1:
-        KE_avgs = np.nan
-        KE_stds = np.nan
-        KE_cost = np.nan
-        return i, j, KE_cost, KE_avgs, KE_stds, np.full(n_samples, np.nan)    
-    
     # running the objective function
     non_spring_info = [c2,c3]
     [KE_cost, KE_avgs, KE_stds, KE_ratios] = objective.objective(non_spring_info, stochastic_info, n_samples)
@@ -74,11 +58,10 @@ def init_worker(shared_data):
 ############################################################
 def get_n_cores():
     n_cores = os.environ.get("SLURM_NTASKS")                # cores from SLURM (HPC)
+    task_id = int(os.environ.get('SLURM_ARRAY_TASK_ID',0))
+    n_jobs = int(os.environ.get("SLURM_ARRAY_TASK_COUNT",1))
 
-    if n_cores is not None:                                 # if this is an HPC Job, use number of cpus from SLURM
-        return int(n_cores)
-    else:
-        return max(1, os.cpu_count() - 1)                   # if no slurm cpu allocation (i.e. running on a local laptop) use local CPU # -1 for background processes
+    return int(n_cores), task_id, n_jobs
 
 ############################################################
 # MAIN
@@ -86,49 +69,39 @@ def get_n_cores():
 if __name__ == '__main__':
     tic = time.perf_counter()                               # counts seconds
 
-    n_cores = get_n_cores()                                 # get number of cores
-
+    n_cores, task_id, n_jobs = get_n_cores()                # get number of cores and tasks and jobs for multinodal computing
     ############################################################
     # random input space defined - CAN CHANGE n_samples
     ############################################################
     n_samples = 1
-    rng = np.random.default_rng()
+    seed = 13510249453205735037716673912871003318           # seed for random number replication
+    rng = np.random.default_rng(seed=seed)
+    
     M_up = 0.45 + 0.015
     M_low = 0.45 - 0.015
-    #M_normal = rng.uniform(M_low, M_up, n_samples)         # varying inputs
-
-    M_normal = [0.05]                                       #single input normalized mass M/M0
+    M_normal = rng.uniform(M_low, M_up, n_samples)          # varying inputs
+    #M_normal = [0.05]                                      #single input normalized mass M/M0
 
     V_up = 0.8+0.3
     V_low = 0.8-0.3
-    #V_normal = rng.uniform(V_low, V_up, n_samples)         # varing inputs
-    V_normal = [1]                                          # single input normalized velocity V/V0
+    V_normal = rng.uniform(V_low, V_up, n_samples)          # varing inputs
+    #V_normal = [1]                                         # single input normalized velocity V/V0
 
     Z_up = 0.15+0.01
     Z_low = 0.15-0.01
-    #zeta_sweep = rng.uniform(Z_low,Z_up,n_samples)         # varing inputs
-    zeta_sweep = [0.01]                                     # single input
+    zeta_sweep = rng.uniform(Z_low,Z_up,n_samples)          # varing inputs
+    #zeta_sweep = [0.01]                                    # single input
+    
     stochastic_info = [M_normal, V_normal, zeta_sweep]
 
     ############################################################
     # Defining C2 C3 grid coarseness
     ############################################################
-    refinement_factor = 1.3333                              # so max c_nums = 500 after 10 steps (i=0:9)
-    #coarse = 20
+    refinement_factor = (500/20)**(1/3)                                # so max c_nums = 500 after 4 steps (i=0:3)
     coarse = 20
-    n_steps = 1
+    n_steps = 4
     c_nums = [int(np.round(coarse*(refinement_factor**i))) for i in range(n_steps)]  
 
-    ############################################################
-    # Array Objects to store results for each coarseness
-    ############################################################
-    total_KE_cost   = np.empty(n_steps, dtype=object)
-    total_KE_avgs   = np.empty(n_steps, dtype=object)
-    total_KE_stds   = np.empty(n_steps, dtype=object)
-    total_KE_ratios = np.empty(n_steps, dtype=object)
-    opt_c2s = np.zeros(n_steps)
-    opt_c3s = np.zeros(n_steps)
-    opt_KEs = np.zeros((n_steps, 3))                                  # each row = [opt_cost, opt KE_avg, opt_std]
     ############################################################
     # run objective function
     ############################################################
@@ -139,6 +112,9 @@ if __name__ == '__main__':
             c2_sweep = np.linspace(-10,-1,c_num)                   # can change ranges
             c3_sweep = np.linspace(1,15,c_num)                     # can change ranges
 
+            c2_slice = np.array_split(c2_sweep, n_jobs)[task_id]
+            c2_indices = np.array_split(c_nums, n_jobs)[task_id]
+            
             ############################################################
             # empty results arrays
             ############################################################
@@ -148,47 +124,34 @@ if __name__ == '__main__':
             KE_ratios = np.zeros((c_num,c_num, n_samples))
 
             tasks = [                                               # defining what is being parallelized over
-                (i, j, c2_sweep[i], c3_sweep[j], n_samples) 
-                for i in range(c_num)
-                for j in range(c_num)
+                (i_global, j_local, slice[i_global], c3_sweep[j_local], n_samples) 
+                for i_global in c2_indices
+                for j_local in range(c_num)
             ]
 
             results = pool.starmap(run_obj, tasks)
 
-            for i, j, cost, avg, std, ratio in results:             # getting the results
-                KE_cost[i,j]   = cost
-                KE_avgs[i,j]   = avg
-                KE_stds[i,j]   = std
-                KE_ratios[i,j] = ratio
-
-            total_KE_cost[k]   = KE_cost
-            total_KE_avgs[k]   = KE_avgs
-            total_KE_stds[k]   = KE_stds
-            total_KE_ratios[k] = KE_ratios
-            ############################################################
-            # get optimal values
-            ############################################################
-            c2_idx,c3_idx = np.unravel_index(np.nanargmin(KE_cost), KE_cost.shape)      # where the optimal spring coefficients are
-            opt_c2s[k] = c2_sweep[c2_idx]
-            opt_c3s[k] = c3_sweep[c3_idx]
-            opt_KEs[k] = [np.nanmin(KE_cost), KE_avgs[c2_idx,c3_idx], KE_stds[c2_idx,c3_idx]]
-
-    toc = time.perf_counter()                                # getting final time
-    print(f"runtime = {toc-tic:.3f}")
+            for i_global, j_local, cost, avg, std, ratio in results:             # getting the results
+                KE_cost[i_global,j_local]   = cost
+                KE_avgs[i_global,j_local]   = avg
+                KE_stds[i_global,j_local]   = std
+                KE_ratios[i_global,j_local] = ratio
 
     ############################################################
     # Saving data
     ############################################################
-    
+    toc = time.perf_counter()                                # getting final time
+    print(f"runtime = {toc-tic:.3f}")
+
     timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")  # saving the date and time
     save_dir = f"results_{timestamp}"
     os.makedirs(save_dir, exist_ok=True)
 
     # saving all desired outputs
-    np.save(os.path.join(save_dir, "total_KE_avgs.npy"),   total_KE_avgs)
-    np.save(os.path.join(save_dir, "total_KE_stds.npy"),   total_KE_stds)
-    np.save(os.path.join(save_dir, "total_KE_cost.npy"),   total_KE_cost)
-    np.save(os.path.join(save_dir, "total_KE_ratios.npy"), total_KE_ratios)
+    np.save(os.path.join(save_dir, f"KE_avgs_ref{k}_slice{task_id}.npy"),   KE_avgs)
+    np.save(os.path.join(save_dir, "total_KE_stds.npy"),   KE_stds)
+    np.save(os.path.join(save_dir, "total_KE_cost.npy"),   KE_cost)
+    np.save(os.path.join(save_dir, "total_KE_ratios.npy"), KE_ratios)
 
     # saving coefficient options
     np.save(os.path.join(save_dir, "c_nums.npy"),  c_nums)
@@ -196,8 +159,7 @@ if __name__ == '__main__':
     # printing all necessary info to the .out file
     print(f"timestamp: {timestamp}")
     print(f"n_cores: {n_cores}")
+    print(f"n_nodes: {n_jobs}")
+    print(f"task_id: {task_id}")
     print(f"n_samples: {n_samples}")
-    for k in range(n_steps):
-        print(f"refinement level {k}: c2 = {opt_c2s[k]:.4f}, c3 = {opt_c3s[k]:.4f}, ")
-        print(f"KE_cost = {opt_KEs[k,0]:.4f}, KE_avg = {opt_KEs[k,1]:.4f}, KE_std = {opt_KEs[k,2]:.4f}")
-
+    
