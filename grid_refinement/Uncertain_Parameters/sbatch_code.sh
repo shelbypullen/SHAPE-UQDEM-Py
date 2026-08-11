4385a6120dbc7158d462bfb42f2423adcc9bc294
Shelby Pullen <spullen@ucsd.edu>
2026-08-11 14:51:52 -0700

    rearranged some things and started on multinode script

--- grid_refinement/Uncertain_Parameters/sbatch_code.sh
+++ grid_refinement/Uncertain_Parameters/sbatch_code.sh
@@ -2,13 +2,18 @@
 #SBATCH --job-name=DEM_Python_grid_refinement
 #SBATCH --output=results_%j.out
 #SBATCH --error=results_%j.err
+
 #SBATCH --nodes=1
 #SBATCH --ntasks=112
 #SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=112
 #SBATCH --time=01:30:00
+
 #SBATCH --partition=pbatch
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=spullen@ucsd.edu
+
+#SBATCH --array=0-49
 
 cd $SLURM_SUBMIT_DIR
 
---
+++ grid_refinement/deterministic_results/sbatch_code.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=DEM_Python_grid_refinement
+#SBATCH --output=results_%j.out
+#SBATCH --error=results_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=112
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --time=01:30:00
+#SBATCH --partition=pbatch
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=spullen@ucsd.edu
+
+cd $SLURM_SUBMIT_DIR
+
+echo "Cores: $SLURM_NTASKS"
+echo "Python: $(which python3)" 
+
+python3 main_grid_rob.py 
