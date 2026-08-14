f67165df6aeb00ba64b2fdd0e13e1fca9329ded4
Shelby Pullen <spullen@ucsd.edu>
2026-08-14 11:53:34 -0700

    final check done

--- grid_refinement/Uncertain_Parameters/main_grid_rob.py
+++ grid_refinement/Uncertain_Parameters/main_grid_rob.py
@@ -64,7 +64,7 @@ def get_n_cores():
     n_cores = os.environ.get("SLURM_NTASKS")                # cores from SLURM (HPC)
     task_id = int(os.environ.get('SLURM_ARRAY_TASK_ID',0))  # which nodal task is it defaults to 0 for 1st index
     n_jobs = int(os.environ.get("SLURM_ARRAY_TASK_COUNT",1))# how many nodes/tasks are there defaults to 1 for 1 job/node
-    job_id = int(os.environ.get("SLURM_ARRAY_JOB_ID",1))
+    job_id = int(os.environ.get("SLURM_ARRAY_JOB_ID",3))
     
     if n_cores is not None:                                 # if this is an HPC Job, use number of cpus from SLURM
         return int(n_cores), task_id, n_jobs, job_id
@@ -97,7 +97,7 @@ if __name__ == '__main__':
     ############################################################
     # random input space defined - CAN CHANGE n_samples
     ############################################################
-    n_samples = 1000
+    n_samples = 2
     seed = 13510249453205735037716673912871003318           # seed for random number replication
     rng = np.random.default_rng(seed=seed)
     
