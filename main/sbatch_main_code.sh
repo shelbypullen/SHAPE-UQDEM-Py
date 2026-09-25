653d1bc9f9acaea772ba90f5d396ac4269784f07
Shelby Pullen <spullen@ucsd.edu>
2026-09-25 13:04:01 -0700

    reorganization and correct .gitignore

---
+++ main/sbatch_main_code.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=20_by_20_DEM_MC
+#SBATCH --output=results_main_%A_%a.out
+#SBATCH --error=results_main_%A_%a.err
+
+#SBATCH --array=0-29
+#SBATCH --nodes=1
+#SBATCH --ntasks=112
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=112
+
+#SBATCH --mem=150G
+#SBATCH --time=08:00:00
+
+#SBATCH --partition=pbatch
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=spullen@ucsd.edu
+
+cd $SLURM_SUBMIT_DIR
+
+echo "Python: $(which python3)" 
+echo "Cores: $SLURM_NTASKS"
+echo "Job ID: $SLURM_ARRAY_JOB_ID"
+echo "Task ID: $SLURM_ARRAY_TASK_ID"
+
+python3 200main_grid_rob.py #include directory name as second arg if previously failed
+
+
